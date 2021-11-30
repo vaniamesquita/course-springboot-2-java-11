ef58556dffc43475e1d44021e04f6b5f8390ebcd
vaniamesquita <ninha.mesquita@gmail.com>
2021-11-30 15:07:31 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,11 +1,16 @@
 package com.educandoweb.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.services.UserService;
 
 /* para saber se o rest da aplicação springboot esta funcionando, criamos um recurso basico baseado na classe user
 	(UserResource - classe que disponiliza um recurso web correspondente a entidade User)
@@ -16,12 +21,25 @@ import com.educandoweb.course.entities.User;
 @RequestMapping(value = "/users")
 public class UserResource { //metodo que vai ser um endpoint para acessar os usuários
 	
+	//dependencia para o service
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() { //generics, espera um tipo dentro, e o tipo da resposta eh a classe user <User>
-		User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() { //generics, espera um tipo dentro, e o tipo da resposta eh a classe user <User>
+		List<User> list = service.findAll();
+				
+		return ResponseEntity.ok().body(list);
 	
 	}
+	
+	//endpoint para buscar um usuário por id
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	
 }
 	
---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,30 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	//dependencia para o UserRespository
+	@Autowired
+	private UserRepository repository;
+	
+	//metodo para retornar todos os usuarios do banco de dados
+	public List<User> findAll() { //operação na camada de serviço
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional <User> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+}
